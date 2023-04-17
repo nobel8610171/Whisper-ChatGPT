8d8fd5eeea6c0a8409b6709c1acc7eca6d199a87
黃冠博 <xingjuan@huanggunbodeMBP.lan>
2023-04-18 01:30:54 +0800

    Updates

---
+++ precache-manifest.77be4fe25d9b4da6b989dca66368a888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6c5824c30084801337",
+    "url": "/Whisper-ChatGPT/static/css/main.a5709ff5.chunk.css"
+  },
+  {
+    "revision": "ff6c5824c30084801337",
+    "url": "/Whisper-ChatGPT/static/js/main.ff6c5824.chunk.js"
+  },
+  {
+    "revision": "9cf301ae317767194c66",
+    "url": "/Whisper-ChatGPT/static/js/1.9cf301ae.chunk.js"
+  },
+  {
+    "revision": "189aa842bad43a371ba8",
+    "url": "/Whisper-ChatGPT/static/js/2.189aa842.chunk.js"
+  },
+  {
+    "revision": "223b353af865cd65a73a",
+    "url": "/Whisper-ChatGPT/static/js/runtime~main.223b353a.js"
+  },
+  {
+    "revision": "08721e49f150e78d3badcfef86741e67",
+    "url": "/Whisper-ChatGPT/index.html"
+  }
+];
